189c83cabf1ba38ceb1630e7bdaa49c0359b3473
Shahmirze <sahmirze.memmedyarov@gmail.com>
2023-10-01 16:44:30 +0400

    fixed news

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,43 +1,66 @@
-import React from 'react';
-import MuiCard from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import Fade from 'react-reveal/Fade';
-const CustomCard = ({title,text,image}) => {
+import React, { useEffect, useReducer } from "react";
+import MuiCard from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import Fade from "react-reveal/Fade";
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "get-data":
+      if (action.payload.length >= 159) {
+        return { ...state, cutText: action.payload.substring(0, 159) };
+      }
+      return { ...state, cutText: action.payload };
+    case "toggle-details":
+      return { ...state, showDetails: !state.showDetails };
+    default:
+      return state;
+  }
+};
+
+const CustomCard = ({ title, text, image }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    showDetails: false,
+    cutText: text.substring(0, 159),
+  });
+
+  const { showDetails, cutText } = state;
 
   return (
     <Fade bottom>
-    <MuiCard sx={{ maxWidth: 280 }} className='my-3'>
-      <a href="#" style={{textDecoration:"none"}}>
-      <CardActionArea>
-        {/* <CardMedia
-          component="img"
-          height="140"
-          image={`http://127.0.0.1:8000/assets/image/news/${image}`} 
-          alt="green iguana"
-        /> */}
-        <img src={`http://127.0.0.1:8000/assets/image/news/${image}`} style={{width: "100%",
-    height: "173px"}} alt="" />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           {text}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-      </a>
-    </MuiCard>
+      <MuiCard sx={{ maxWidth: 280 }} className="my-3">
+        <CardActionArea>
+          <img
+            src={`http://127.0.0.1:8000/assets/image/news/${image}`}
+            style={{ width: "100%", height: "173px" }}
+            alt=""
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {showDetails ? text : cutText}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          {text.length > 159 && (
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => dispatch({ type: "toggle-details" })}
+            >
+              {showDetails ? "Less Details" : "More Details"}
+            </Button>
+          )}
+        </CardActions>
+      </MuiCard>
     </Fade>
   );
 };
 
 export default CustomCard;
+
--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,28 +1,27 @@
-import React, { useContext, useEffect, useReducer } from 'react'
-import { PageConatiner } from '../PageConatiner'
-import '../assets/scss/news.scss'
+import React, { useContext, useEffect, useReducer } from "react";
+import { PageConatiner } from "../PageConatiner";
+import "../assets/scss/news.scss";
 import Linkedin from "../assets/image/linkedin.png";
 import Reddit from "../assets/image/reddit.png";
 import Twiter from "../assets/image/twiter.png";
 import { AiOutlineArrowDown } from "react-icons/ai";
-import { Fade } from 'react-reveal';
-import MyContact from '../components/contact';
-import CustomCard from '../components/card';
-import api from '../api/api';
-import { useNavigate } from 'react-router-dom';
+import { Fade } from "react-reveal";
+import MyContact from "../components/contact";
+import CustomCard from "../components/card";
+import api from "../api/api";
+import { useNavigate } from "react-router-dom";
 
-const reducer=(state,action)=>{
-  switch(action.type){
-    case'get-data':
-    return action.payload
-    break;
-
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "get-data":
+      return action.payload;
+      break;
   }
-}
+};
 
 const News = () => {
-  const navigate= useNavigate()
-  const [data,dispatch]=useReducer(reducer,[])
+  const navigate = useNavigate();
+  const [data, dispatch] = useReducer(reducer, []);
 
   const MyIcons = [
     {
@@ -38,34 +37,40 @@ const News = () => {
       text: "Twiter",
     },
   ];
- 
-  
-  useEffect(()=>{
-    api.get('/news').then(({news})=>dispatch({type:'get-data',payload:news}))
-  },[])
-  return (  
+
+  useEffect(() => {
+    api
+      .get("/news")
+      .then(({ news }) => dispatch({ type: "get-data", payload: news }))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
     <PageConatiner>
       <div>
-       <div className="title">
-        <div className="container">
-          <button onClick={handleCount}>+</button>
-          <h1>Are You Aware of It ?</h1>
+        <div className="title">
+          <div className="container">
+            <h1>Are You Aware of It ?</h1>
+          </div>
         </div>
-        
-       </div>
       </div>
-      
-      <MyContact text="red"/>
-      <div className="container d-flex justify-content-center myContent gap-3 my-7" id='red'>
-       
-       {data.map((elem,index)=>
-        <CustomCard key={index} text={elem.description} title={elem.title}  image={elem.image}/>
-       )} 
-      
+
+      <MyContact text="red" />
+      <div
+        className="container d-flex justify-content-center myContent gap-3 my-7"
+        id="red"
+      >
+        {data.map((elem, index) => (
+          <CustomCard
+            key={index}
+            text={elem.description}
+            title={elem.title}
+            image={elem.image}
+          />
+        ))}
       </div>
     </PageConatiner>
-    
-  )
-}
+  );
+};
 
-export default News
+export default News;
